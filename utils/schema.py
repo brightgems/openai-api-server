d83f4fdd2b00625af173797765ad6d529a0f7508
george.pan <george.pan@unilever.com>
2023-03-09 23:02:09 +0800

    auth by agile token

--- utils/schema.py
+++ utils/schema.py
@@ -1,17 +1,22 @@
 
 from pydantic import BaseModel
-
+import requests
+from config import JWT_SECRET_KEY
 
 users = [("openaiDriver", "hope&poem")]
 
 
 class AuthSettings(BaseModel):
-    authjwt_secret_key: str = "Jy771qqJWmvQN7c7oU1"
+    authjwt_secret_key: str = JWT_SECRET_KEY
     algorithm = "HS256"
 
     @classmethod
     def is_authenticated(cls, username, password):
-        return username.count("@unilever.com")
+        is_authenticated = username.count("@unilever.com") and password == JWT_SECRET_KEY
+        if not is_authenticated and password:
+            resp = requests.get('https://cmiai-agileinnovation.unilever-china.com/api/v1/me',
+                                headers={"Content-Type": "application/json", "Authorization": "Bearer "+password})
+            return resp.status_code == 200
 
 
 class User(BaseModel):
